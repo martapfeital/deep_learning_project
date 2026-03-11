e5c3337fc753552c1fb07c82a0ec68cff42a4c24
Marta <marta.p.feital03@gmail.com>
2026-03-11 12:59:16 +0000

    Update notebook and dataset download script

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -1,23 +1,17 @@
 import os
 import zipfile
-import requests
+import gdown
 
-DATASET_URL = "https://liveeduisegiunl-my.sharepoint.com/:u:/g/personal/amarques_novaims_unl_pt/IQCcfwZTCUBRSKZg4wIQ9DOSAdA4fqruEHl5Ssx9dAGoprc?e=WSWtKf"
+FILE_ID = "1No8kSNf_-JYXHtKGCH1anFi191HPZtrA"
 
 DATA_DIR = "data"
 ZIP_PATH = os.path.join(DATA_DIR, "wikiart.zip")
 EXTRACT_DIR = os.path.join(DATA_DIR, "wikiart")
 
-def download_file(url, dest_path):
+def download_file(file_id, dest_path):
     print("Downloading dataset...")
-    response = requests.get(url, stream=True)
-    response.raise_for_status()
-
-    with open(dest_path, "wb") as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            if chunk:
-                f.write(chunk)
-
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, dest_path, quiet=False)
     print("Download finished.")
 
 def extract_zip(zip_path, extract_to):
@@ -33,7 +27,7 @@ def main():
         print("Dataset already exists. Nothing to do.")
         return
 
-    download_file(DATASET_URL, ZIP_PATH)
+    download_file(FILE_ID, ZIP_PATH)
     extract_zip(ZIP_PATH, DATA_DIR)
 
     os.remove(ZIP_PATH)
